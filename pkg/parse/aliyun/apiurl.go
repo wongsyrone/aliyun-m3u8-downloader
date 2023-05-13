afff93cc82f03f2209b5b2875ca1fd5f8bc4be3f
lbbniu <lbbniu@gmail.com>
2023-05-14 00:30:43 +0800

    修改 PlayConfig 参数

--- pkg/parse/aliyun/apiurl.go
+++ pkg/parse/aliyun/apiurl.go
@@ -78,8 +78,8 @@ func GetPlayInfoRequestUrl(rand, playAuth, videoId string, opts ...OptionFunc) (
 	privateParams["AuthInfo"], _ = sj.Get("AuthInfo").String()
 	privateParams["AuthTimeout"] = "7200"
 	privateParams["Definition"] = "240" //gk 此参数为空
-	privateParams["PlayConfig"] = "{}"
-	privateParams["PlayerVersion"] = "2.8.2"
+	privateParams["PlayConfig"] = `{"EncryptType":"AliyunVoDEncryption"}`
+	privateParams["PlayerVersion"] = "2.9.0"
 	privateParams["ReAuthInfo"] = "{}"
 	privateParams["SecurityToken"], _ = sj.Get("SecurityToken").String()
 	privateParams["VideoId"] = videoId
