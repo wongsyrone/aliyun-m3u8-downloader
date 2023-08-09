4e54bcd16c96c2e658bb087774e8fdfbf29be87e
lbbniu <lbbniu@gmail.com>
2023-08-09 18:28:00 +0800

    perf decrypFunc

--- pkg/download/dowloader.go
+++ pkg/download/dowloader.go
@@ -287,7 +287,13 @@ func (d *Downloader) download(segIndex int) error {
 			// 自定义解密函数
 			tsData, err = d.decryptFunc(segIndex, fPath, tsData, sf, keyInfo)
 			if err != nil {
-				return fmt.Errorf("download: decryptFunc: %s, err: %w", tsUrl, err)
+				// TODO: 如果当前片段时长大于1秒，则认为解密失败
+				seg := d.segment(segIndex)
+				if seg.Duration > 1 {
+					return fmt.Errorf("download: decryptFunc: %s, err: %w", tsUrl, err)
+				}
+				log.Errorf("segment: %+v, err: %v", seg, tsUrl, err)
+				tsData = nil
 			}
 		} else if keyInfo.AliyunVoDEncryption {
 			// 阿里云私有加密
@@ -444,6 +450,10 @@ func (d *Downloader) mergeTsToMp4ByGo(mFilePath string) error {
 func (d *Downloader) tsURL(segIndex int) string {
 	seg := d.result.M3u8.Segments[segIndex]
 	return tool.ResolveURL(d.result.URL, seg.URI)
+}
+
+func (d *Downloader) segment(segIndex int) *parse.Segment {
+	return d.result.M3u8.Segments[segIndex]
 }
 
 func (d *Downloader) tsFilename(tsUrl string) string {
